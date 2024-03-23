721fafca1eb4310e420629169e792bdb097935dc
Reuben John <reubenvjohn@gmail.com>
2024-03-23 23:19:43 +0000

    Implemented ability to abort chat web socket

--- main/views/companion/chat.py
+++ main/views/companion/chat.py
@@ -30,8 +30,9 @@ AGENT_NAME = "Agent"
 class ChatConsumer(AsyncWebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.cancel_response_requested = False
         self.user_id = None
+        self.agent_executor = None
+        self.respond_task = None
 
     async def connect(self):
         self.user_id = UserId(self.scope["url_route"]["kwargs"]["user_id"])
@@ -40,7 +41,15 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
-        logging.info(f"Websocket for '{self.user_id}'; code: {close_code}")
+        logging.info(f"User '{self.user_id}' disconnected chat websocket ({close_code})")
+        await self.stop()
+
+    async def stop(self):
+        logging.info(f"User {self.user_id} stopping response")
+        if self.agent_executor:
+            self.agent_executor.max_iterations = 0
+        if self.respond_task:
+            await self.respond_task
 
     async def receive(self, text_data: str):
         try:
@@ -48,11 +57,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
             cmd = json_data["command"]
             if cmd == "close":
                 logging.info(f"User {self.user_id} requested websocket close")
-                self.cancel_response_requested = True
-                await self.close()
+                await self.close()  # Triggers self.disconnect(...)
             elif cmd == "respond":
                 user_input = json_data["payload"]
-                asyncio.create_task(self.respond_to_user(self.user_id, user_input))
+                self.respond_task = asyncio.create_task(self.respond_to_user(self.user_id, user_input))
         except Exception as e:
             logging.error(e)
             await self.send(f"\nAn Error occurred: {str(e)}")
@@ -72,49 +80,37 @@ class ChatConsumer(AsyncWebsocketConsumer):
         tools = [bind_query_tasks(user_id), get_items, where_cat_is_hiding]
 
         # agent = get_assistant(tools)
-        agent = create_openai_tools_agent(
-            model.with_config({"tags": ["agent_llm"]}), tools, prompt
-        )
-        agent_executor = AgentExecutor(agent=agent, tools=tools).with_config(
+        agent = create_openai_tools_agent(model.with_config({"tags": ["agent_llm"]}), tools, prompt)
+        self.agent_executor = AgentExecutor(agent=agent, tools=tools)
+        runnable = self.agent_executor.with_config(
             {"run_name": AGENT_NAME},
         )
 
-        async for event in agent_executor.astream_events(
+        async for event in runnable.astream_events(
             {
                 "input": user_input,
             },
             version="v1",
         ):
-            if self.cancel_response_requested:
-                break
-            await self.send(json.dumps(event, cls=StrJSONEncoder))
             kind = event["event"]
-            if kind == "on_chain_start":
-                if event["name"] == AGENT_NAME:
-                    print(
-                        f"Starting agent: {event['name']} with input: {event['data'].get('input')}"
-                    )
-            elif kind == "on_chain_end":
-                if event["name"] == AGENT_NAME:
-                    print()
-                    print("--")
-                    print(
-                        f"Done agent: {event['name']} with output: {event['data'].get('output')['output']}"
-                    )
-                    return
-            elif kind == "on_chat_model_stream":
+            event_name = event["name"]
+
+            if self.agent_executor.max_iterations == 0:
+                break
+            # if event_name == AGENT_NAME and kind == "on_chain_start": # event['data'].get('input')
+            if event_name == AGENT_NAME and kind == "on_chain_end":
+                break  # Agent execution completed
+
+            if kind == "on_chat_model_stream":
                 content = event["data"]["chunk"].content
                 if content:
                     # Empty content in the context of OpenAI means
                     # that the model is asking for a tool to be invoked.
                     # So we only print non-empty content
-                    print(content, end="|")
+                    await self.send(f"|{content}")
             elif kind == "on_tool_start":
-                print("--")
-                print(
-                    f"Starting tool: {event['name']} with inputs: {event['data'].get('input')}"
-                )
+                data = dict(toolName=event_name, input=event["data"].get("input"))
+                await self.send(f"{kind}|{json.dumps(data)}")
             elif kind == "on_tool_end":
-                print(f"Done tool: {event['name']}")
-                print(f"Tool output was: {event['data'].get('output')}")
-                print("--")
+                data = dict(toolName=event_name, output=event["data"].get("output"))
+                await self.send(f"{kind}|{json.dumps(data)}")
