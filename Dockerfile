ad736406dcb9aa02c313a2e9df7f60fb3bb6f80c
Reuben John <reubenvjohn@gmail.com>
2024-03-24 22:49:19 +0000

    Run DB migrations during docker creation + reset database schema

--- Dockerfile
+++ Dockerfile
@@ -11,5 +11,7 @@ COPY . .
 EXPOSE 8000
 
 RUN chmod +x containers_python_django/startup.sh
+RUN python manage.py makemigrations
+RUN python manage.py migrate
 
 CMD [ "./containers_python_django/startup.sh" ]
--- containers_python_django/startup.sh
+++ containers_python_django/startup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Gets the details from the environment variables (see .env.example)
 python manage.py createsuperuser --noinput
+# Execute createsuperuser transaction on database along with any other pending migrations
 python manage.py migrate
+
 daphne containers_python_django.asgi:application --bind 0.0.0.0 -p 8000
